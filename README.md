c6be0d4b7cdba83dc5f2eda9cf1915b5afbfa0e4
Kevin Denver <kevin.denver@gmail.com>
2021-05-28 09:29:01 +0100

    Add script to create new games

--- README.md
+++ README.md
@@ -10,6 +10,35 @@ See https://github.com/conflictserpent/battlesnake-server-2018
 
 ![battlesnake-uis](screenshot-game.jpg)
 
-## Configuration
+## Local Development of your Snake
+
+### Manual Configuration
+
+```shell
+# 1) run the battlesnake-server on localhost:5000
+make run-battlesnake-server
+
+# 2) Start your Snake on your local machine on a port (e.g.: 7070)
+
+# 3) Create a new game in the UI
+open http://localhost:5000/new
+
+# 4) Enter your snakes url: http://host.docker.internal:7070 
+```
+
+### Scripted
+
+```shell
+# 1) run the battlesnake-server on localhost:5000
+make run-battlesnake-server
+
+# 2) Add your Snake's name and url to a text file 
+# dev-snake,http://host.docker.internal:9090
+
+# 3) Run the script
+cd scripts && make new-game CONFIG=starter.snakes.txt
+```
+
+### New Game Dialog
 
 ![battlesnake-uis](screenshot-new-game.jpg)
---
+++ scripts/new-game/index.js
@@ -0,0 +1,60 @@
+const {chromium} = require('playwright');
+
+function readInputFromStdin(callback) {
+    let input = '';
+
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('data', data => input += data);
+    process.stdin.on('end', () => {
+        const parsed = input.split("\n")
+            .filter(it => it)
+            .map(it => it.split(",").map(it => it.trim()))
+            .map(it => {
+                return {'name': it[0], 'host': it[1]}
+            });
+        callback(parsed);
+    });
+}
+
+async function setupNewGame(snakes) {
+    async function configureSnake(name, host, idx) {
+        await page.fill(`#game_form_snakes_${idx}_url`, host)
+        await page.fill(`#game_form_snakes_${idx}_name`, name)
+    }
+
+    async function configureBoardSize(width, height) {
+        await page.fill('#game_form_width', width.toString());
+        await page.fill('#game_form_height', height.toString());
+    }
+
+    async function configureMaxFood(amount) {
+        await page.fill('#game_form_max_food', amount.toString());
+    }
+
+    const browser = await chromium.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('http://localhost:5000/new');
+
+    await configureBoardSize(20, 20);
+    await configureMaxFood(3);
+    for (const snake of snakes) {
+        const idx = snakes.indexOf(snake);
+        await configureSnake(snake.name, snake.host, idx);
+    }
+
+    await page.click("button");
+    await page.click("text=Show");
+    const gameUrl = await page.url();
+    await browser.close();
+
+    return gameUrl;
+}
+
+readInputFromStdin(snakes => {
+    (async () => {
+        const gameUrl = await setupNewGame(snakes);
+        console.log(`New Game created at ${gameUrl}`)
+    })();
+});
