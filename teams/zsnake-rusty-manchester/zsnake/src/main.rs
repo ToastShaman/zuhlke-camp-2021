9e907ad826a3cbe930657067bbcf0e16e684ad41
David Wylie <david.wylie@zuhlke.com>
2021-06-11 12:19:39 +0100

    added rusty to docker compose

--- teams/zsnake-rusty-manchester/zsnake/src/main.rs
+++ teams/zsnake-rusty-manchester/zsnake/src/main.rs
@@ -4,10 +4,10 @@ use warp::Filter;
 async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+        .map(|name| format!("Hello Again, {}!", name));
 
     warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
+        .run(([0,0,0,0], 3030))
         .await;
 }
 
